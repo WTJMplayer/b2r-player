02e1406a97867ff4f55ce854a6708328937af0aa
jmaraya1229 <jessemaraya2013@gmail.com>
2022-05-14 14:07:53 -0700

    unfinished navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,12 @@
 import { Link } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import { HiOutlineMusicNote } from 'react-icons/fa';
 
 function Navbar() {
     const [click, setClick] = useState(false);
 
     const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
     return (
         <>
         <nav className="navbar">
@@ -12,11 +14,20 @@ function Navbar() {
             <div className="navbar-container">
                 <Link to="/" className="navbar-logo">
                     {/* put image link in to="/" */}
-                    <h1>Look homepage link thing</h1>
+                    {/* <i> <HiOutlineMusicNote /> */}
+                    {/* </i> */}
+                    <h1> Bach 2 Rock </h1>
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                 </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                </ul>
             </div>
         
         </nav>
