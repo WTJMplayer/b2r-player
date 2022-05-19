82b09e54b8ec610dc0e850fc545e266b2ef9c73a
jmaraya1229 <jessemaraya2013@gmail.com>
2022-05-18 17:06:14 -0700

    added Carousel

--- client/src/components/PlaylistCards.js
+++ client/src/components/PlaylistCards.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './PlaylistCards.css';
 import PlaylistCardItem from './PlaylistCardItem';
+import Carousel from 'nuka-carousel';
+
 
 // Fills in playlist cards. Need to figure out how to set up playlist
 function PlaylistCards() {
@@ -10,24 +12,46 @@ function PlaylistCards() {
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
-            <PlaylistCardItem
-              src='images/fillerimage.jpg'
-              text='Text describing playlist 1'
-              label='Playlist 1'
-              path='/dashboard'
-            />
-            <PlaylistCardItem
-              src='images/fillerimage.jpg'
-              text='Text describing playlist 2'
-              label='Playlist 2'
-              path='/dashboard'
-            />
-            <PlaylistCardItem
-              src='images/fillerimage.jpg'
-              text='Text describing playlist 3'
-              label='Playlist 3'
-              path='/dashboard'
-            />
+            <Carousel
+            slidesToShow={3}
+            swiping={true}>
+                <PlaylistCardItem
+                src='images/fillerimage.jpg'
+                text='Text describing playlist 1'
+                label='Playlist 1'
+                path='/dashboard'
+                />
+                <PlaylistCardItem
+                src='images/fillerimage.jpg'
+                text='Text describing playlist 2'
+                label='Playlist 2'
+                path='/dashboard'
+                />
+                <PlaylistCardItem
+                src='images/fillerimage.jpg'
+                text='Text describing playlist 3'
+                label='Playlist 3'
+                path='/dashboard'
+                />
+                <PlaylistCardItem
+                src='images/fillerimage.jpg'
+                text='Text describing playlist 4'
+                label='Playlist 4'
+                path='/dashboard'
+                />
+                <PlaylistCardItem
+                src='images/fillerimage.jpg'
+                text='Text describing playlist 5'
+                label='Playlist 5'
+                path='/dashboard'
+                />
+                <PlaylistCardItem
+                src='images/fillerimage.jpg'
+                text='Text describing playlist 5'
+                label='Playlist 5'
+                path='/dashboard'
+                />
+            </Carousel>
           </ul>
         </div>
       </div>
--- client/src/components/SongCards.js
+++ client/src/components/SongCards.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './SongCards.css';
 import SongCardItem from './SongCardItem';
-
+import Carousel from 'nuka-carousel';
 
 // What fills in the song cards, need to figure out how to link to songs from database
 function SongCards() {
@@ -11,6 +11,11 @@ function SongCards() {
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
+          <Carousel
+            slidesToShow={3}
+            // wrapAround={true}
+            swiping={true}
+            >
             <SongCardItem
               src='images/fillerimage.jpg'
               text='Song name 1'
@@ -25,10 +30,29 @@ function SongCards() {
             />
             <SongCardItem
               src='images/fillerimage.jpg'
-              text='song name 3'
-              label='Artist 3'
+              text='song name 4'
+              label='Artist 4'
               path='/dashboard'
             />
+            <SongCardItem
+              src='images/fillerimage.jpg'
+              text='song name 5'
+              label='Artist 5'
+              path='/dashboard'
+            />
+            <SongCardItem
+              src='images/fillerimage.jpg'
+              text='song name 6'
+              label='Artist 6'
+              path='/dashboard'
+            />
+            <SongCardItem
+              src='images/fillerimage.jpg'
+              text='song name 7'
+              label='Artist 7'
+              path='/dashboard'
+            />
+            </Carousel> 
           </ul>
         </div>
       </div>
