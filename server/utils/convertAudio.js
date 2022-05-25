249144b17787493abd6a3ac540a0c54ea3edeafa
willt <91817538+swillswindle@users.noreply.github.com>
2022-05-24 19:07:37 -0700

    done

--- server/utils/convertAudio.js
+++ server/utils/convertAudio.js
@@ -1,7 +1,4 @@
 const ffmpeg = require("fluent-ffmpeg");
-ffmpeg.setFfmpegPath("./utils/ffmpeg/bin/ffmpeg.exe");
-ffmpeg.setFfprobePath("./utils/ffmpeg/bin/ffprobe.exe");
-const fs = require("fs");
 //convert a file
 
 // ffmpeg()
